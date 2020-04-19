75eb3402b7b894aacea295afa14e5341e2635446
bobaxix <marcin.beberok@gmail.com>
2020-04-19 18:54:37 +0200

    Add dynamic rendering for paymentView (rerender when payment removed)

--- typescript/src/delete.ts
+++ typescript/src/delete.ts
@@ -7,28 +7,46 @@ class MainController {
         const removeBtns = document.getElementsByClassName('remove-buttons')
 
         Array.from(removeBtns)
-            .forEach(el => el.addEventListener('click', ev => {
-                const btn = ev.target as HTMLButtonElement
-                this.removeService.removeItem(btn.id)
-                console.log(btn.id)
+            .forEach(btn => btn.addEventListener('click', ev => {
+                
+                removeService.removeItem(btn.id)
+
+                let tableRow = btn.parentElement!.parentElement!
+                let tableBody = tableRow!.parentElement!
+
+                tableRow?.remove()
+
+                if (tableBody?.childElementCount === 2) {
+                    tableBody.remove()
+                } else {
+
+                    let moneyPerRow = tableRow.querySelectorAll('.money')!
+                    let moneyPerBody = tableBody.querySelectorAll('.money')!
+
+                    let summaryCell = Array.from(moneyPerBody).pop() as HTMLTableCellElement
+
+                    const oldVal = this.getValue(summaryCell.textContent!)
+                    const removedVal = this.getValue(moneyPerRow[0].textContent!)
+                    const newVal = oldVal - removedVal
+                    
+                    summaryCell.textContent! = `${newVal.toFixed(2)} zł`
+                    this.updateId(tableBody)
+                }
+                
+                
             }))
+    }
 
-        // document.getElementById('modal-btn')
-        //     ?.addEventListener('click', () => {
-        //         let modalStyle = document.getElementById('search-modal')?.style
+    getValue(el: String) {
+          return Number(el?.split(' ')[0])
 
-        //         if (modalStyle)
-        //         if (modalStyle)
-        //             modalStyle.display = 'block'
-        // })
+    }
 
-        // window.onclick = (ev : MouseEvent) => {
-        //     let modal = document.getElementById('search-modal')
-        //     if (ev.target == modal) {
-        //         if (modal?.style)
-        //             modal.style.display = 'none'
-        //     }
-        // }
+    updateId(tableBody: HTMLElement) {
+        let idPerBody = tableBody.querySelectorAll('.id')
+        for (let i = 0; i < idPerBody.length; i++) {
+            idPerBody[i].textContent = String(i + 1)
+        }
     }
 
 }
